21f3a954ba4430d61d8564b63eb2fbee2fa48811
maula iqbal <rifqimaula@gmail.com>
2025-12-22 14:23:58 +0700

    'bug fix'

--- app/Http/Resources/Project/ProjectResource.php
+++ app/Http/Resources/Project/ProjectResource.php
@@ -20,7 +20,7 @@ class ProjectResource extends JsonResource
             'name' => $this->name,
             'description' => $this->description,
             'favorite' => $this->favorite,
-            'client_company' => $this->clientCompany->only(['id', 'name']),
+            'client_company' => $this->clientCompany ? $this->clientCompany->only(['id', 'name']) : null,
             'users_with_access' => PermissionService::usersWithAccessToProject($this),
             'all_tasks_count' => $this->all_tasks_count,
             'completed_tasks_count' => $this->completed_tasks_count,
