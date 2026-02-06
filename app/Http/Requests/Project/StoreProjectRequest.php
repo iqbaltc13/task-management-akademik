d3891b9790577d6e399e49cd118e5e3a0f882b61
maula iqbal <rifqimaula@gmail.com>
2026-02-06 09:28:03 +0700

    ' CRUD Project : hapus form input select institusi'

--- app/Http/Requests/Project/StoreProjectRequest.php
+++ app/Http/Requests/Project/StoreProjectRequest.php
@@ -26,8 +26,8 @@ class StoreProjectRequest extends FormRequest
         return [
             'name' => ['required', 'string', Rule::unique('projects', 'name')],
             'description' => 'string|nullable',
-            'default_pricing_type' => ['required', 'string', Rule::enum(PricingType::class)],
-            'client_company_id' => 'required|exists:client_companies,id',
+            'default_pricing_type' => ['nullable', 'string', Rule::enum(PricingType::class)],
+            'client_company_id' => 'nullable|exists:client_companies,id',
             'rate' => 'numeric|min:0|nullable',
             'users' => 'array',
         ];
