8c441cbcc6edab7e8d692fad4c38243d6aae7870
maula iqbal <rifqimaula@gmail.com>
2026-02-06 11:36:26 +0700

    'Service : fixing Permission Service'

--- app/Services/PermissionService.php
+++ app/Services/PermissionService.php
@@ -11,21 +11,23 @@ class PermissionService
 {
     public static $permissionsByRole = [
         'admin' => [
-            'User' => ['view users', 'view user rate', 'create user', 'edit user', 'archive user', 'restore user'],
-            'Label' => ['view labels', 'create label', 'edit label', 'archive label', 'restore label'],
-            'Role' => ['view roles', 'create role', 'edit role', 'archive role', 'restore role'],
-            'Owner Company' => ['view owner company', 'edit owner company'],
-            'Client User' => ['view client users', 'create client user', 'edit client user', 'archive client user', 'restore client user'],
-            'Client Company' => ['view client companies', 'create client company', 'edit client company', 'archive client company', 'restore client company'],
-            'Project' => ['view projects', 'view project', 'create project', 'edit project', 'archive project', 'restore project', 'edit project user access'],
-            'TaskGroups' => ['create task group', 'edit task group', 'archive task group', 'restore task group', 'reorder task group'],
+            'User' => ['lihat pengguna', 'lihat rate pengguna', 'buat pengguna', 'edit pengguna', 'pengguna terhapus', 'kembalikan pengguna'],
+            'Label' => ['lihat label', 'buat label', 'edit label', 'label terhapus', 'kembalikan label'],
+            'Role' => ['lihat peran', 'buat peran', 'edit peran', 'peran terhapus', 'kembalikan peran'],
+            'Owner Company' => ['lihat perusahaan pemilik', 'edit perusahaan pemilik'],
+            'Client User' => ['lihat pengguna client', 'buat pengguna client', 'edit pengguna client', 'pengguna client terhapus', 'kembalikan pengguna client'],
+            //'Client Company' => ['lihat perusahaan client', 'buat perusahaan client', 'edit perusahaan client', 'perusahaan client terhapus', 'kembalikan perusahaan client'],
+            'Project' => ['lihat proyek', 'lihat proyek detail', 'buat proyek', 'edit proyek', 'proyek terhapus', 'kembalikan proyek', 'edit akses pengguna proyek'],
+            'TaskGroups' => ['buat grup permintaan', 'edit grup permintaan', 'grup permintaan terhapus', 'kembalikan grup permintaan', 'urutan grup permintaan'],
             'Tasks' => [
-                'view tasks', 'create task', 'edit task', 'archive task', 'restore task', 'reorder task', 'complete task', 'add time log', 'delete time log',
-                'view time logs', 'view comments',
+                'lihat permintaan', 'buat permintaan', 'edit permintaan', 'arsip permintaan', 'kembalikan permintaan', 'urutan permintaan', 'selesaikan permintaan', 'tambah log waktu', 'hapus log waktu',  
+                'lihat log waktu', 'lihat komentar',
             ],
-            'Invoices' => ['view invoices', 'create invoice', 'edit invoice', 'archive invoice', 'restore invoice', 'change invoice status', 'download invoice', 'print invoice'],
-            'Reports' => ['view logged time sum report', 'view daily logged time report', 'view fixed price sum report'],
-            'Activities' => ['view activities'],
+            //'Invoices' => ['view invoices', 'create invoice', 'edit invoice', 'archive invoice', 'restore invoice', 'change invoice status', 'download invoice', 'print invoice'],
+            //'Reports' => ['lihat laporan  ringkasan waktu total ', 'lihat laporan  waktu harian', 'lihat laporan  total harga fix'],
+            'Reports' => ['lihat laporan  ringkasan waktu total ', 'lihat laporan  waktu harian'],
+            
+            'Activities' => ['lihat aktifitas'],
         ],
         'helpdesk' => [
             //'User' => ['view users', 'view user rate', 'create user', 'edit user', 'archive user', 'restore user'],
