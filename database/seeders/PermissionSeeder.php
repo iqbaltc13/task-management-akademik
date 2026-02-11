9da7399929b49ec8b27927f67778c5d1e3293214
maula iqbal <rifqimaula@gmail.com>
2026-02-11 10:39:24 +0700

    oke

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -18,6 +18,7 @@ class PermissionSeeder extends Seeder
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
         $insertPermissions = fn ($role) => collect(PermissionService::$permissionsByRole[$role])
+                ->collapse()
                 ->map(function ($item) {
                 dd($item);
                 $permission = DB::table('permissions')->where('name', $item['name'])->first();
