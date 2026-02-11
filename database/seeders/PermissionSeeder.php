b50d65cbea2b4f70fb6d617d6e0f0d7050f7b257
maula iqbal <rifqimaula@gmail.com>
2026-02-11 10:55:01 +0700

    bug fix collection

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -16,11 +16,11 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
-        //dd(collect(PermissionService::$permissionsByRole['admin'])->collapse());
+        
         $insertPermissions = fn ($role) => collect(PermissionService::$permissionsByRole[$role])
                 ->collapse()
                 ->map(function ($item) {
-                dd($item);
+                
                 $permission = DB::table('permissions')->where('name', $item['name'])->first();
 
                 return $permission
