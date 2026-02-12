cc50d10bf6db3ef9b3bfb940ba9b0fae779c33e7
maula iqbal <rifqimaula@gmail.com>
2026-02-12 13:59:04 +0700

    role permissions edit

--- resources/js/pages/Settings/Roles/Edit.jsx
+++ resources/js/pages/Settings/Roles/Edit.jsx
@@ -81,12 +81,12 @@ const RoleEdit = () => {
                 <Group justify="start" gap="sm">
                   {allPermissionsGrouped[group].map((permission) => (
                     <Chip
-                      key={permission}
+                      key={permission.name}
                       radius="sm"
-                      checked={form.data.permissions.includes(permission)}
-                      onClick={() => toggle(permission)}
+                      checked={form.data.permissions.includes(permission.name)}
+                      onClick={() => toggle(permission.name)}
                     >
-                      {permission}
+                      {permission.title}
                     </Chip>
                   ))}
                 </Group>
