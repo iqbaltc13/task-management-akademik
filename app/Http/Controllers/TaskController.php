e2051a3e2a47f8efd3fbcd29316e7b28a3faceb4
maula iqbal <rifqimaula@gmail.com>
2026-09-10 13:40:10 +0700

    card task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -62,7 +62,7 @@ class TaskController extends Controller
         return Inertia::render('Projects/Tasks/Index', [
             'project' => $project,
             'usersWithAccessToProject' => PermissionService::usersWithAccessToProject($project),
-            'labels' => Label::get(['id', 'name', 'color']),
+            'labels' => Label::get(['id', 'name', 'color', 'code', 'identity_number']),
             'taskGroups' => $groups,
             'jobTitles' => $jobTitles,
             'groupedTasks' => $groupedTasks,
